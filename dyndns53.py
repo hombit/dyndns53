6d6cc50b994def9c22da2b88a3dc2b9ebceb198b
Konstantin Malanchev <hombit@gmail.com>
2020-04-15 12:05:55 +0300

    Refactoring before add new DNS provider

--- dyndns53.py
+++ dyndns53.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
+import logging
+from copy import deepcopy
 
 import boto3
-from copy import deepcopy
 
 
 GET_IP_URLS=(
@@ -16,50 +16,59 @@ GET_IP_URLS=(
 
 
 def _get_ip():
-    from requests import get
+    import requests
+
     for url in GET_IP_URLS:
         try:
-            return get(url).text
-        except:
-            pass
-    raise(RuntimeError('Cannot get IP'))
+            return requests.get(url).text
+        except requests.exceptions.RequestException as e:
+            logging.info(str(e))
+    raise RuntimeError('Cannot get IP')
 
 
-def dyndns(hostname, ip=None):
+def route53(hostname, ip=None):
     if ip is None:
         ip = _get_ip()
 
     roothostname = '.'.join( hostname.split('.')[-2:] )
+    recordname = hostname + '.'
 
     client = boto3.client('route53')
     Id = client.list_hosted_zones_by_name(DNSName=roothostname, MaxItems='1')['HostedZones'][0]['Id']
     current_record = client.list_resource_record_sets(
         HostedZoneId=Id,
-        StartRecordName=hostname+'.',
+        StartRecordName=recordname,
         StartRecordType='A',
         MaxItems='1'
     )['ResourceRecordSets'][0]
+
+    if current_record['Name'] != recordname:
+        msg = "Hostname {} isn't found on Route53".format(hostname)
+        logging.error(msg)
+        raise ValueError(msg)
+
     current_ip = current_record['ResourceRecords'][0]['Value']
 
-    if ip != current_ip:
-        new_record = deepcopy(current_record)
-        new_record['ResourceRecords'][0]['Value'] = ip
+    if ip == current_ip:
+        logging.info("Skipping update for {}, IP hasn't been changed".format(hostname))
+        return
 
-        response = client.change_resource_record_sets(
-            HostedZoneId=Id,
-            ChangeBatch={
-                'Comment': 'Automatically update A record for host {}'.format(hostname),
-                'Changes': [
-                    {
-                        'Action': 'UPSERT',
-                        'ResourceRecordSet': new_record,
-                    }
-                ]
-            }
-        )
-        return response
-    else:
-        return "Skipping update, IP hasn't been changed"
+    new_record = deepcopy(current_record)
+    new_record['ResourceRecords'][0]['Value'] = ip
+
+    response = client.change_resource_record_sets(
+        HostedZoneId=Id,
+        ChangeBatch={
+            'Comment': 'Automatically update A record for host {}'.format(hostname),
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': new_record,
+                }
+            ]
+        }
+    )
+    logging.info('{} address is updated to {}, response: {}'.format(hostname, ip, str(response)))
 
 
 ################################################################################
@@ -67,12 +76,22 @@ def dyndns(hostname, ip=None):
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--ip', '-i', type=str,
                         help='Manually set IP, default is to set public IP of the host')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='Verbose logging, default is ERROR, -v is WARNING, -vv is INFO')
     parser.add_argument('hostname', type=str, metavar='HOST', nargs='+',
                         help='Hostname to update')
     args = parser.parse_args()
 
+    logging_level = logging.INFO
+    if args.verbose == 0:
+        logging_level = logging.ERROR
+    elif args.verbose == 1:
+        logging_level= logging.WARNING
+    logging.basicConfig(level=logging_level)
+
     for hostname in args.hostname:
-        print(dyndns(hostname, ip=args.ip))
+        route53(hostname, ip=args.ip)
